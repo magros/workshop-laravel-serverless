f7276a48dfc916dd6871daf49ca6f3af1237511e
Marco <marco@empleosti.com.mx>
2021-05-28 16:50:28 -0500

    add route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hola', function (\Illuminate\Http\Request $request) {
+    return "Hola ".$request->get('name');
+});
